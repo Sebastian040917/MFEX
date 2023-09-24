009d8cfe18fc5af774147e464789f0f77ac0e11d
Sebastian FV <sebastianfernandezvergara@hotmail.com>
2023-09-23 23:42:48 -0600

    Chagges on test.ts 3

--- projects/commons-lib/src/test.ts
+++ projects/commons-lib/src/test.ts
@@ -15,8 +15,8 @@ declare const require: {
   };
 };
 
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().forEach(context);
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
--- projects/mf-shell/src/test.ts
+++ projects/mf-shell/src/test.ts
@@ -8,6 +8,14 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 
+declare const require: {
+  context(path: string, deep?: boolean, filter?: RegExp): {
+    <T>(id: string): T;
+    keys(): string[];
+  };
+};
+
+
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
